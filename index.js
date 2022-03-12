d4a69696c8a1318c68153d79073212f68a319723
Amarjeet Sahoo <amarjeethappysahoo@gmail.com>
2022-03-12 15:15:14 +0530

    added:limit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use("/api", restroRoute);
 app.use("/api/restaurant", categoryRoute);
 app.use("/api/admin", adminLoginRoute);
